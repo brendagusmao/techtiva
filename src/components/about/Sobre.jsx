0d07b401a3fc53ad65ccd6f53b5ee610c4a752fe
Brenda Pereira Gusmão <brendapgusmao@gmail.com>
2023-05-09 21:08:22 -0300

    feat: header

--- src/components/about/Sobre.jsx
+++ src/components/about/Sobre.jsx
@@ -4,7 +4,6 @@ function Sobre() {
       <section>
         <h1> A parceria que faltava para seu negócio decolar</h1>
         <desc>
-        Nossa missão é desenvolver soluções digitais para impulsor os resultados dos nossos clientes<p/>
         </desc>
       </section>
   );
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -19,10 +19,10 @@ function Header() {
     <Nav />
       <header id="home">
         <div className="sidesc desc">
-          <h1> Transforme seu negócio</h1>
+          <h1>  A parceria que faltava para seu negócio decolar</h1>
           <section className="description">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          </section>
+          Nossa missão é desenvolver soluções digitais para impulsor os resultados dos nossos clientes<p/>
+        </section>
           <button type='button'>
             saiba mais <i><FaArrowRight  /></i>
           </button>
