96d99302dc4afe1c2675f0314861e6a10016aa94
brendagusmao <brendapgusmao@gmail.com>
2023-05-24 00:10:32 -0300

    feat: section projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,8 @@ import Oi from './oi';
 function Projects() {
     return (
     <div className="Projects" id="projects">
+        <h1> Nossos Projetos</h1>
+        <h2> Confira abaixo nossos modelos</h2>
         <section className="gridprojects">
         <Oi />
         </section>
--- src/pages/oi.jsx
+++ src/pages/oi.jsx
@@ -1,15 +1,20 @@
 import { Image } from 'antd';
+import Image1 from '../images/Rectangle (2).png';
+// import Image2 from '../images/Rectangle.png';
+
 const App = () => (
+  <div className="gridprojects">
   <Image.PreviewGroup
     preview={{
       onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
     }}
   >
-    <Image width={200} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" />
-    <Image
-      width={200}
-      src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
-    />
+    <div className="oioi"><Image width={320} src="https://i.pinimg.com/originals/ac/97/d1/ac97d16bb7e22802b051da75c417b30f.jpg" /> </div>
+    <div className="oioi"><Image width={320} src="https://s.tmimgcdn.com/scr/53600/isp-responsive-landing-page-template_53666-original.jpg" /></div>
+    <div className="oioi"><Image width={320} src="https://i.pinimg.com/originals/2b/25/af/2b25af6f83615b2a1aa67968f0a22415.png" /></div>
+    <div className="oioi"><Image width={320} src={Image1} /></div>
+    <div className="oioi"><Image width={320} src={Image1} /></div>
   </Image.PreviewGroup>
+  </div>
 );
 export default App;
