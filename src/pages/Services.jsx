6a65fdf44b5235498cd9345838ae5937fc11c452
brendagusmao <brendapgusmao@gmail.com>
2023-06-21 00:40:34 -0300

    feat: efeitos de entrada

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,29 +1,42 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import FormSale from '../components/forms/FormSale';
 import { Modal } from 'antd';
 import { useState } from 'react';
 import {BsCheck2Circle} from 'react-icons/bs';
 import MyContext from '../context/MyContext';
 import Sales from '../components/services/Sales';
+import { debounce, animeScroll } from '../components/scrollUtils';
 import '../css/Services.css';
 
 function Services() {
   const {className} = useContext(MyContext);
   const [modal1Open, setModal1Open] = useState(false);
   const [modal2Open, setModal2Open] = useState(false);
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      animeScroll();
+    }, 200);
 
+    animeScroll();
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return  ( 
     <div className="Services" id="services">
-      <section className={className ? 'main' : 'main visible'}>
+      <section className={className ? 'main' : 'main visible'} data-anime="top">
         <h1 className="titlee">Construa um site de sucesso que reflete a excelência da sua empresa! Escolha um dos nossos planos e destaque-se na web.
 </h1>
       <Sales />
       </section>
       <section className='maintree'>
-        <section className='feat width'>
+        <section className='feat width' data-anime="left">
         <img src="https://o.remove.bg/downloads/05ed821e-fb6d-457d-a2b4-531995c5db39/11-Best-Web-Design-Companies-in-India-removebg-preview.png" alt="" width={'100%'} />
         </section>
-        <div className='feat'>
+        <div className='feat' data-anime="right">
         <h1>Construa uma presença online poderosa e cativante</h1>
         <desc>A primeira impressão importa, especialmente no mundo digital. Nossos templates profissionais e designs modernos irão cativar seus visitantes e transformá-los em clientes fiéis. Com foco em usabilidade e experiência do usuário, nossas páginas garantem uma navegação intuitiva e uma apresentação visualmente impressionante.
         </desc>
